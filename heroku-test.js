88f2b10044dd9546210813ff34347fe70b9080d2
Pete Jones <pwj2012@gmail.com>
2020-09-16 16:12:11 -0700

    hey

--- heroku-test.js
+++ heroku-test.js
@@ -2,15 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), err => {
-    if (err) res.status(500).send(err)
-  })
-})
-// app.get('/', (req, res) => {
-//   res.send('is this thing on?')
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'), err => {
+//     if (err) res.status(500).send(err)
+//   })
 // })
+app.get('/', (req, res) => {
+  res.send('is this thing on?')
+})
 
 module.exports = port => {
   app.listen(port)

7c57f636ee5f75e1ca86d676c7450bfdcb47716c
Pete Jones <pwj2012@gmail.com>
2020-09-16 17:06:43 -0700

    work?

--- heroku-test.js
+++ heroku-test.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const TEST_ENV_VAR = process.env.TEST_ENV_VAR
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'reacc/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), err => {
+  res.sendFile(path.join(__dirname, '/reacc/build/index.html'), err => {
     if (err) res.status(500).send(err)
   })
 })
 
 app.get('/', (req, res) => {
-  res.send('is this thing on?')
+  res.send('is this thing on?', TEST_ENV_VAR)
 })
 
 if (process.env.NODE_ENV === 'production') {

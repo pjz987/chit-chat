22b322e80cf9da476c6b8d7831baa43e4efa3ad5
Pete Jones <pwj2012@gmail.com>
2020-09-16 17:00:01 -0700

    work?

--- heroku-test.js
+++ heroku-test.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'), err => {
     if (err) res.status(500).send(err)
@@ -12,6 +13,10 @@ app.get('*', (req, res) => {
 app.get('/', (req, res) => {
   res.send('is this thing on?')
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('reacc/build'))
+}
 
 module.exports = port => {
   app.listen(port)

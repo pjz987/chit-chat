ba411a5f89b8065f287a95e171c789eed6738a33
Pete Jones <pwj2012@gmail.com>
2020-09-18 09:13:53 -0700

    created backend branch for aws deployment

--- reacc/src/components/LogOut.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-
-export default function LogOut (props) {
-  props.logOut()
-  return <Redirect to='/' />
-}
--- reacc/src/handlers.js
+++
@@ -1,9 +0,0 @@
-function createUsernameHandler (el, body, state) {
-  return function () {
-    state.name = document.querySelector('#username_input').value
-    body.removeChild(el)
-    state.display_.style.display = 'block'
-  }
-}
-
-export default createUsernameHandler
